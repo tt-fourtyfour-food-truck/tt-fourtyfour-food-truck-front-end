9aac8b261084acf4e24eda0886dc90395c56ed99
alexismarroquin7 <alexismarroquin754@gmail.com>
2021-03-29 11:12:35 -0700

    removes unnecessary files

---
+++ src/actions/appActions.js
@@ -0,0 +1,7 @@
+
+
+export const ADD_TRUCK = "ADD_TRUCK";
+export const DELETE_TRUCK = "DELETE_TRUCK";
+export const UPDATE_TRUCK = "UPDATE_TRUCK";
+
+
--- src/index.js
+++ src/index.js
@@ -8,10 +8,6 @@ import logger from "redux-logger";
 import thunk from "redux-thunk";
 import rootReducer from "./reducers";
 
-//style
-import App from './App';
-import './index.css';
-
 const middleware = applyMiddleware(thunk, logger);
 const store = createStore(rootReducer, middleware);
 
