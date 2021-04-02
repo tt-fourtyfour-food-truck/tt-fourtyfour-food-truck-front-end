d35a195443ecb30dead2a19495deba364fc23ba2
Will B <wberman27@gmail.com>
2021-04-02 09:45:35 -0400

    add view-trucks 2

--- src/components/BrowseTrucks.js
+++ src/components/BrowseTrucks.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
 // import {Link} from 'react-router-dom'
 import {makeStyles} from '@material-ui/core/styles'
 import Grid from '@material-ui/core/Grid'
