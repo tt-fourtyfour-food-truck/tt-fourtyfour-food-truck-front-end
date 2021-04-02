3a51b7b50eb8bd599007549689c8637cbdf84bad
Will B <wberman27@gmail.com>
2021-04-02 15:06:58 -0400

    update app reducers

--- src/store/reducers/AppReducer.js
+++ src/store/reducers/AppReducer.js
@@ -13,6 +13,7 @@ const initialState = {
         trucksOwned: [
             {
                 truck: {
+                    id: '',
                     img: '',
                     cuisineType: '',
                     customerRatings: [],
@@ -40,28 +41,75 @@ const initialState = {
 export const AppReducer = (state = initialState, action) => {
     switch(action.type){
         case ADD_TRUCK:
-            return state;
+            return {
+                ...state,
+                operator: {...state.operator, trucksOwned: [...state.trucksOwned, action.payload]}
+            }
 
         case UPDATE_TRUCK:
-            return state;
+            return {
+                ...state,
+                operator: {...state.operator, 
+                    trucksOwned: state.trucksOwned.map((truck)=>{
+                    if(truck.id === action.payload.id){
+                        const updatedTruck = Object.assign(truck, action.payload)
+                        return updatedTruck
+                    }else
+                    {
+                        return truck
+                    }
+                })}
+                
+            }
 
         case DELETE_TRUCK:
-            return state;
+            return {
+                ...state,
+                operator: {...state.operator, 
+                    trucksOwned: state.trucksOwned.filter((truck => truck.id !== action.payload.id))}
+                
+            }
 
         case CREATE_MENU_ITEM:
-            return state;
+            return{
+                ...state,
+                menuItems: action.payload,
+                menu: [...menu, action.payload]
+            }
 
         case UPDATE_MENU_ITEM:
-            return state;
+            return {
+                ...state,
+                operator: {...state.operator.trucksOwned.truck, 
+                    menu: state.menu.map((menuItem)=>{
+                    if(menuItem.id === action.payload.id){
+                        const updatedMenu = Object.assign(menuItem, action.payload)
+                        return updatedMenu
+                    }else
+                    {
+                        return menuItem
+                    }
+                })}
+                
+            }
 
         case UPDATE_DINER_INFO:
-            return state;
+            return {
+                ...state,
+                diner: Object.assign(diner, action.payload)
+            }
 
         case UPDATE_OPERATOR_INFO:
-            return state;
+            return {
+                ...state,
+                operator: Object.assign(operator, action.payload)
+            }
         
         case ADD_TRUCK_TO_FAVORITES:
-            return state;
+            return {
+                ...state,
+                diner: {...state.diner, favoriteTrucks: [...state.diner.favoriteTrucks, action.payload]}
+            }
        
         default:
             return state;
